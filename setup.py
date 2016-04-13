b4fbc9b71020db38bbaab598e292578c090de5ec
spence dalley <spencedalley@users.noreply.github.com>
2016-04-12 18:18:07 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+try: 
+	from setuptools import setup 
+except ImportError: 
+	from distutils.core import setup 
+
+config = {
+    'description': 'Project creation utility script',
+    'author': 'Spence',
+    'url': 'URL',
+    'download_url': 'DOWNLOAD_URL',
+    'author_email': 'EMAIL',
+    'version': '0.1.0',
+    'install_requires': ['nose', 'gitpython',],
+    'packages': ['fig'],
+    'scripts': [],
+    'name': 'fig'
+}
+
+setup(**config)
---
+++ tests/fig_tests.py
@@ -0,0 +1,11 @@
+from nose.tools import * 
+import fig 
+
+def setup():
+	print("SETUP!")
+
+def teardown():
+	print("TEARDOWN!")
+
+def test_basic():
+	print("I RAN!")
