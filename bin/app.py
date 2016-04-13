d968ba6891b704cca3402a9742851fa2a6aa1588
spence dalley <spencedalley@users.noreply.github.com>
2016-04-12 23:29:59 -0700

    Copy over skeletons and modify contents

---
+++ bin/app.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+
+import git
+import os
+import shutil 
+import sys
+
+from fig import project_conf
+
+ROOT_PATH = os.path.dirname(os.path.realpath(__file__))[:-4]
+
+PATHS = PATHS = {
+	'c': ROOT_PATH + '/skeletons/c-skeleton',
+	'python': ROOT_PATH + '/skeletons/python-skeleton',
+	'ruby': ROOT_PATH + '/skeletons/ruby-skeleton',
+}
+
+def main(): 
+	if len(sys.argv) < 3: 
+		print('USAGE: app.py <language> <project_name>')
+		sys.exit(0)
+
+	language = sys.argv[1]
+	project_name = sys.argv[2]
+	project_path = os.getcwd() + '/' + project_name
+	project_skeleton = PATHS.get(language, None)
+
+	if project_skeleton == None: 
+		print('Invalid language specified. Currently supported languages are:')
+		for key in PATHS.keys(): 
+			print('- %s' % key)
+		exit(0)
+
+	if os.path.isdir(project_path) == True: 
+		print('There is already a a directory with the project name specified in the current directory')
+		print('Choose a different name for you project.')
+		sys.exit(0)
+
+	# copy over the skeleton and do version control
+	shutil.copytree(project_skeleton, project_path)
+	git.Repo.init(project_path)
+	
+	if language == 'python':
+		print("In python") 
+		project_conf.python_project(project_path, project_name)
+	elif language is 'ruby': 
+		print("In ruby")
+		project_conf.ruby_project(project_path, project_name)
+	elif language is 'c': 
+		print("In c")
+		project_conf.c_project(project_path, project_name)
+	else: 
+		pass
+
+if __name__ == '__main__': 
+	main()
---
+++ fig/project_conf.py
@@ -0,0 +1,68 @@
+import os
+
+def python_project(project_path, project_name):
+	os.rename(project_path+'/NAME', project_path+'/'+project_name)
+	os.rename(project_path+'/tests/NAME_tests.py',
+			  project_path+'/tests/%s_tests.py' % project_name)
+
+	# change the file contents
+	test_contents = open(project_path+'/tests/%s_tests.py' % project_name).read()
+	test_contents = test_contents.replace('NAME', project_name)
+
+	with open(project_path+'/tests/%s_tests.py' % project_name, 'w') as f:
+		f.write(test_contents)
+
+	setup_contents = open(project_path+'/setup.py').read()
+	setup_contents = setup_contents.replace('NAME', project_name)
+
+	with open(project_path+'/setup.py', 'w') as f:
+		f.write(setup_contents)
+
+	return
+
+def ruby_project(project_path, project_name):
+	os.rename(project_path+'/bin/NAME', project_path+'/bin/%s' % project_name)
+	os.rename(project_path+'/lib/NAME', project_path+'/lib/%s' % project_name)
+	os.rename(project_path+'/lib/NAME.rb', project_path+'/lib/%s.rb' % project_name)
+	os.rename(project_path+'/NAME.gemspec', project_path+'/%s.gemspec' % project_name)
+	os.rename(project_path+'/tests/test_NAME.rb', project_path+'/tests/test_%s.rb' % project_name)
+
+	test_contents = open(project_path+'/tests/test_%s.rb' % project_name).read()
+	test_contents = test_contents.replace('NAME', project_name)
+
+	# change file contents
+	with open(project_path+'/tests/test_%s.rb' % project_name, 'w') as f:
+		f.write(test_contents)
+
+	gemspec = open(project_path+'/%s.gemspec' % project_name).read()
+	gemspec = gemspec.replace('NAME', project_name)
+
+	with open(project_path+'/%s.gemspec' % project_name, 'w') as f:
+		f.write(gemspec)
+
+	return
+
+def c_project(project_path, project_name):
+	os.rename(project_path+'/src/NAME.c', project_path+'/src/%s.c' % project_name)
+    os.rename(project_path+'/src/NAME.h', project_path+'src/%s.h' % project_name)
+    os.rename(project_path+'/tests/lib_tests.c', project_path+'tests/%s_tests.c' % project_name)
+
+    test_contents = open(project_path+'/tests/%s_tests.c').read()
+    test_contents = test_contents.replace('NAME', project_name)
+
+    with open(project_path+'/tests/%s_tests.c', 'w') as f:
+        f.write(tests_contents)
+
+    src_contents = open(project_path+'/src/%s.h' % project_name).read()
+    src_contents = src_contents.replace('NAME', project_name)
+
+    with open(project_path+'/src/%s.h' % project_name, 'w') as f:
+        f.write(src_contents)
+
+    makefile = open(project_path+'/Makefile').read()
+    makefile = makefile.replace('NAME', project_name)
+
+    with open(project_path+'/Makefile', 'w') as f:
+        f.write(makefile)
+
+    return 
