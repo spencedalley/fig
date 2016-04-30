c1b89d7ac03cc170a6c70c0e0b0685c2f8fcad6b
spence dalley <spencedalley@users.noreply.github.com>
2016-04-30 12:49:12 -0700

    Removed bin from packages

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ config = {
     'author_email': 'EMAIL',
     'version': '0.1.0',
     'install_requires': ['nose', 'gitpython',],
-    'packages': ['fig', 'bin'],
+    'packages': ['fig'],
     'scripts': [],
     'name': 'fig'
 }
